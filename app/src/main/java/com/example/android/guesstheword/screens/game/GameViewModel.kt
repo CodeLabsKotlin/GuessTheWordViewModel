2c54cef7d33d1d81efef46710084e67fc9a18d82
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-10-16 17:20:24 +0100

    Añadimos LiveData a la vista-modelo de la clase Game

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,6 +17,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 /**
@@ -25,10 +26,9 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
 
     // The current word
-    var word = ""
-
+    val word = MutableLiveData<String>()
     // The current score
-    var score = 0
+    val score = MutableLiveData<Int>()
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -68,6 +68,8 @@ class GameViewModel : ViewModel() {
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
+        word.value = ""
+        score.value = 0
     }
 
     /**
@@ -80,21 +82,21 @@ class GameViewModel : ViewModel() {
 
     /** Methods for updating the UI **/
     fun onSkip() {
-        score--
-        nextWord()
-    }
-    fun onCorrect() {
-        score++
+        score.value = (score.value)?.minus(1)
         nextWord()
     }
 
+    fun onCorrect() {
+        score.value = (score.value)?.plus(1)
+        nextWord()
+    }
     /**
      * Moves to the next word in the list.
      */
     private fun nextWord() {
-        //Select and remove a word from the list
         if (!wordList.isEmpty()) {
-            word = wordList.removeAt(0)
+            //Select and remove a word from the list
+            word.value = wordList.removeAt(0)
         }
     }
 }
