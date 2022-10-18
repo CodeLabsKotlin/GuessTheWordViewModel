33f9bacd370ca84318195ae61a832843abfefb0f
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-10-18 19:46:05 +0100

    proyecto finalizado - añadido un contador haciendo uso de transformation.map()

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,9 +17,11 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 
@@ -65,6 +67,11 @@ class GameViewModel : ViewModel() {
         get() = _currentTime
 
     private val timer: CountDownTimer
+
+    // The String version of the current time
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
 
 
     /**
